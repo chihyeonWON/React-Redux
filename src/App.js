5c9ac4b9c472966379d4b0436090e90357b577af
원치현 <heungeob1003@gmail.com>
2021-10-24 16:43:49 +0900

    Import AddNumberRoot, DisplayNumberRoot to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import AddNumberRoot from '/components/AddNumberRoot';
+import DisplayNumberRoot from './components/DisplayNumberRoot';
+
 
 function App() {
   return(
