f40a905164042cd4e116e066449115894931bd6e
원치현 <heungeob1003@gmail.com>
2021-10-24 16:41:03 +0900

    Create DisplayNumberRoot.jsx and seperate DisplayNumberRoot Component from App.js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,3 @@ function App() {
     </div>
   );
 }
-class DisplayNumberRoot extends Component {
-  render(){
-    return(
-      <div>
-        <h1>Display Number Root</h1>
-        <DisplayNumber></DisplayNumber>
-      </div>
-    );
-  }
-}
-export default App;
