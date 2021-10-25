a8f982c7c110da6806627076c1d7a55c60a21692
원치현 <heungeob1003@gmail.com>
2021-10-25 16:05:33 +0900

    Add onChange function to print changed value

--- src/components/AddNumber.jsx
+++ src/components/AddNumber.jsx
@@ -9,7 +9,9 @@ export default class AddNumber extends Component {
       <div>
         <h1>Add Number</h1>
         <input type="button" value="+"></input>
-        <input type="text" value="0"></input>
+        <input type="text" value={this.state.size} onChange={function(e){
+          this.setState({ size:e.target.value });
+        }.bind(this)}></input>
       </div>
     );
   }
