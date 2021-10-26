5dc510c65ae9e50130f1cd1e1c24ca9301f5fadf
원치현 <heungeob1003@gmail.com>
2021-10-26 16:42:01 +0900

    Add constructor and store's subscribe method when called store's state is changed

--- src/components/DisplayNumber.jsx
+++ src/components/DisplayNumber.jsx
@@ -4,6 +4,13 @@ export default class DisplayNumber extends Component{
   state = {
     number:store.getState().number
   }
+  // subscribe 함수의 첫번째 인자의 함수는 리덕스 스토어의 값이 변경되었을 때 호출
+  constructor(props){
+    super(props);
+    store.subscribe(function(){
+      this.setState({number:store.getState().number}); 
+    }.bind(this))
+  }
   render(){
     return(
       <div>
