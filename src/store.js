e705714d2bdcd5cbd6ce54693ca68d18e2db4544
원치현 <heungeob1003@gmail.com>
2021-10-26 16:24:38 +0900

    Create Redux store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { createStore } from 'redux';
 
 export default createStore(function(state, action){
-
-
-
-
-
+    if(state === undefined) { //초기상태
+        return {number:0}
+    }
+    if(action.type === 'INCREMENT'){
+        return{...state, number:state.number + action.size}
+    }
     return state;
-})
+}, window.__REDUX_DEVTOOL__EXTENSION__ && window.__REDUX_DEVTOOL__EXTENSION__())
